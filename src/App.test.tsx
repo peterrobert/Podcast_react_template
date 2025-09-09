c99db7a14b4063358c3dac226aafdc8dd88621c9
peterrobert <pwambui93@gmail.com>
2025-09-09 10:45:33 +0300

    Created the tests library

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-it('renders navigation brand', () => {
-  render(<App />)
-  expect(screen.getByText(/EchoWave/i)).toBeInTheDocument()
-})
+it("renders navigation brand", () => {
+  render(<App />);
+  expect(screen.getByText(/EchoWave/i)).toBeInTheDocument();
+});
